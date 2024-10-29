7d0d35f36016b64f860f1787afb322fcb8ea66e5
Lasse Hjorth Madsen <lassehjorthmadsen@gmail.com>
2024-10-29 15:52:35 +0100

    edit scripts for network update

--- scripts/build_research_network.R
+++ scripts/build_research_network.R
@@ -26,7 +26,11 @@ research_bundle <- build_network(key_actor = key_actor,
                                  keywords = keywords,
                                  token = token,
                                  refresh_tok = refresh_tok,
-                                 threshold = 0.01)
+                                 threshold = 40,
+                                 save_net = TRUE,
+                                 max_iterations = 30,
+                                 sample_size = Inf,
+                                 file_name = net_file)
 
 # Save the result
 research_bundle$net      |> saveRDS(net_file)
@@ -40,32 +44,48 @@ research_bundle$widget   |> saveWidget(widget_file)
 
 # In case we want to expand an existing net, rather that build a new one
 
-net_file      <- paste0("data/research_net_", Sys.Date(), ".rds")
-profile_file  <- paste0("data/research_profiles_", Sys.Date(), ".csv")
-widget_file   <- paste0("data/research_widget_", Sys.Date(), ".html")
+# To-do: Figure out the best way to do that; maybe just just init_net() on
+# the full set of "left-hand-side" handles in the net to be updated. The
+# idea being, we have a set of "accepted" members, that may have aquired
+# more followings and thus potential new members since last update?
 
-# Get the researcher keywords
-keywords <- read_lines(file = "data/research_keywords.txt")
+threshold    <- 40
+net_file     <- "data/research_net_2024-10-23.rds"
+profile_file <- paste0("data/research_profiles_", Sys.Date(), ".csv")
+widget_file  <- paste0("data/research_widget_", Sys.Date(), ".html")
+keywords     <- read_lines(file = "data/research_keywords.txt")
 
 # Get a saved net
-net <- readRDS("research_net_2024-10-09.rds")
-
-# net <- net |>
-#   dplyr::distinct(.keep_all = TRUE) |>
-#   na.omit()
+net <- readRDS(net_file)
 
 # Expand the net
-# set.seed(100)
 expnet <- expand_net(net = net,
                      keywords = keywords,
                      token = token,
                      refresh_tok = refresh_tok,
                      save_net = FALSE,
-                     file_name = file_name,
-                     threshold = 30,
+                     file_name = net_file,
+                     threshold = threshold,
                      max_iterations = 30,
                      sample_size = Inf)
 
-# Save the result
-expnet |> saveRDS(net_file)
+# Trim the net
+net <- expnet |> trim_net(threshold = threshold)
 
+# Get profiles
+profiles <- get_profiles(unique(net$actor_handle), token)
+
+# Add metrics
+profiles <- profiles |> add_metrics(net)
+
+# Word frequencies in descriptions
+freqs <- profiles$description |> word_freqs()
+
+# Create widget
+widget <- create_widget(net, profiles)
+
+# Save the results
+net      |> saveRDS(net_file)
+profiles |> write.csv2(profile_file, row.names = FALSE)
+profiles |> saveRDS(str_replace(profile_file, "csv", "rds"))
+widget   |> saveWidget(widget_file)
