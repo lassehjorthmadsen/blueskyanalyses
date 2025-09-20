e9bd19313e6dec835d144f9ff7e3a0e1f09a46dd
LMDN <lassehjorthmadsen@gmail.com>
2025-09-20 23:16:34 +0200

    split scripts: build_/expand_research_network.R

--- scripts/build_research_network.R
+++ scripts/build_research_network.R
@@ -4,18 +4,18 @@ library(tidyverse)
 library(htmlwidgets)
 devtools::load_all("../blueskynet")
 
-password       <- Sys.getenv("BLUESKY_APP_PASS")
-identifier     <- Sys.getenv("BLUESKY_APP_USER")
-auth_object    <- get_token(identifier, password)
-token          <- auth_object$accessJwt
-refresh_tok    <- auth_object$refreshJwt
-threshold      <- 0.01 
-max_iterations <- 100
+# Authorization
+password        <- Sys.getenv("BLUESKY_APP_PASS")
+identifier      <- Sys.getenv("BLUESKY_APP_USER")
+auth_object     <- get_token(identifier, password)
+token           <- auth_object$accessJwt
+refresh_tok     <- auth_object$refreshJwt
 
-##########################
-# BUILD RESEARCH NETWORK
-##########################
+# Parameters
+threshold       <- 0.01 
+max_iterations  <- 100
 
+# File locations
 keywords_file   <- "data/research_keywords.txt"
 key_actor       <- "rossdahlke.bsky.social"
 net_file        <- paste0("data/research_net_", Sys.Date(), ".rds")
@@ -23,8 +23,10 @@ profile_file    <- paste0("data/research_profiles_", Sys.Date(), ".csv")
 widget_file     <- paste0("data/research_widget_", Sys.Date(), ".html")
 widget_file_rds <- str_replace(widget_file, ".html", ".rds")
 
+# Key words
 keywords <- read_lines(file = keywords_file)
 
+# Do the work
 research_bundle <- build_network(key_actor = key_actor,
                                  keywords = keywords,
                                  token = token,
@@ -41,56 +43,3 @@ research_bundle$net      |> saveRDS(net_file)
 research_bundle$profiles |> write.csv(profile_file, row.names = FALSE)
 research_bundle$widget   |> saveWidget(widget_file)
 research_bundle$widget   |> saveRDS(widget_file_rds)
-
-###########################
-# RE-BUILD RESEARCH NETWORK
-###########################
-
-# In case we want to expand an existing net, rather that build a new one
-
-# To-do: Figure out the best way to do that; maybe just just init_net() on
-# the full set of "left-hand-side" handles in the net to be updated. The
-# idea being, we have a set of "accepted" members, that may have aquired
-# more followings and thus potential new members since last update?
-
-threshold    <- 40
-net_file     <- "data/research_net_2024-10-31.rds"
-profile_file <- paste0("data/research_profiles_", Sys.Date(), ".csv")
-widget_file  <- paste0("data/research_widget_", Sys.Date(), ".html")
-keywords     <- read_lines(file = "data/research_keywords.txt")
-
-# Get a saved net
-net <- readRDS(net_file)
-
-# Expand the net
-expnet <- expand_net(net = net,
-                     keywords = keywords,
-                     token = token,
-                     refresh_tok = refresh_tok,
-                     save_net = FALSE,
-                     file_name = net_file,
-                     threshold = threshold,
-                     max_iterations = 30,
-                     sample_size = Inf)
-
-# Trim the net
-net <- expnet |> trim_net(threshold = threshold)
-
-# Get profiles
-profiles <- get_profiles(unique(net$actor_handle), token)
-
-# Add metrics
-profiles <- profiles |> add_metrics(net)
-
-# Word frequencies in descriptions
-freqs <- profiles$description |> word_freqs()
-
-# Create widget
-widget <- create_widget(net, profiles, prop = 0.20)
-
-# Save the results
-net      |> saveRDS(net_file)
-profiles |> write.csv2(profile_file, row.names = FALSE)
-profiles |> saveRDS(str_replace(profile_file, "csv", "rds"))
-widget   |> saveWidget(widget_file)
-widget   |> saveRDS(str_replace(widget_file, "html", "rds"))
---
+++ scripts/expand_research_network.R
@@ -0,0 +1,62 @@
+# Expand a network of researchers 
+
+library(tidyverse)
+library(htmlwidgets)
+devtools::load_all("../blueskynet")
+
+password       <- Sys.getenv("BLUESKY_APP_PASS")
+identifier     <- Sys.getenv("BLUESKY_APP_USER")
+auth_object    <- get_token(identifier, password)
+token          <- auth_object$accessJwt
+refresh_tok    <- auth_object$refreshJwt
+threshold      <- 0.01 
+max_iterations <- 100
+
+# In case we want to expand an existing net, rather that build a new one
+
+# To-do: Figure out the best way to do that; maybe just just init_net() on
+# the full set of "left-hand-side" handles in the net to be updated. The
+# idea being, we have a set of "accepted" members, that may have aquired
+# more followings and thus potential new members since last update?
+
+threshold    <- 40
+net_file     <- "data/research_net_2024-10-31.rds"
+profile_file <- paste0("data/research_profiles_", Sys.Date(), ".csv")
+widget_file  <- paste0("data/research_widget_", Sys.Date(), ".html")
+keywords     <- read_lines(file = "data/research_keywords.txt")
+
+# Get a saved net
+net <- readRDS(net_file)
+
+# Expand the net
+expnet <- expand_net(net = net,
+                     keywords = keywords,
+                     token = token,
+                     refresh_tok = refresh_tok,
+                     save_net = FALSE,
+                     file_name = net_file,
+                     threshold = threshold,
+                     max_iterations = 30,
+                     sample_size = Inf)
+
+# Trim the net
+net <- expnet |> trim_net(threshold = threshold)
+
+# Get profiles
+profiles <- get_profiles(unique(net$actor_handle), token)
+
+# Add metrics
+profiles <- profiles |> add_metrics(net)
+
+# Word frequencies in descriptions
+freqs <- profiles$description |> word_freqs()
+
+# Create widget
+widget <- create_widget(net, profiles, prop = 0.20)
+
+# Save the results
+net      |> saveRDS(net_file)
+profiles |> write.csv2(profile_file, row.names = FALSE)
+profiles |> saveRDS(str_replace(profile_file, "csv", "rds"))
+widget   |> saveWidget(widget_file)
+widget   |> saveRDS(str_replace(widget_file, "html", "rds"))
